c70b9c5b2ce056df93f321322a12ae5b0aaacf4c
aryan-byte5 <aryanptn20@gmail.com>
2026-02-17 11:01:21 +0530

    UC1: Add welcome message module

---
+++ src/UC1.java
@@ -0,0 +1,7 @@
+public class UC1 {
+    public static void main(String[] args){
+        System.out.println("Welcome to the palindrome checker management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
+}
---
+++ src/UC3.java
@@ -0,0 +1,17 @@
+public class UC3 {
+
+    public static void main(String[] args) {
+        String original = "radar";
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)) {
+            System.out.println("The string '" + original + "' is a palindrome.");
+        } else {
+            System.out.println("The string '" + original + "' is NOT a palindrome.");
+        }
+    }
+}
